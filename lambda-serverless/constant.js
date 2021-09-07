382e43e075004c5f0b90f71b3fa322488f0a697e
Maksim Tsipun <solcreative@gmail.com>
2021-09-07 22:48:24 +0300

    add new constants

--- lambda-serverless/constant.js
+++ lambda-serverless/constant.js
@@ -2,6 +2,7 @@ module.exports = {
   MAX_USERNAME_LENGTH: 10,
   IP_HEADER: 'X-Envoy-External-Address',
   DRIVER_INIT_TIMEOUT: 10000,
+  RESPONSE_MAX_MESSAGES: 10,
   errors: {
     USER_IS_EXIST: { code: 409, message: 'User with the same name already exist' },
     USER_CHECK_ERROR: { code: 500, message: 'Server Error on user checking operation' },
@@ -14,5 +15,7 @@ module.exports = {
     USER_UPDATE_ERROR: { code: 500, message: 'Server Error on user updating operation' },
     MESSAGE_LENGTH_ERROR: { code: 400, message: 'Message length have to be more then 0 chars and less then 128' },
     MESSAGE_ADD_ERROR: { code: 500, message: 'Server Error on user message posting operation' },
+    MESSAGE_READ_ERROR: { code: 500, message: 'Server Error on user message reading operation' },
+    MESSAGE_NOT_FOUND: { code: 404, message: 'Message not found' },
   }
 };
