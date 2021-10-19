20034296b11e8ce77be13f7bb3e20d3d3c41797a
Maksim Tsipun <solcreative@gmail.com>
2021-10-20 00:42:40 +0300

    Add access validation to message getting

--- lambda-serverless/services/getMessage.js
+++ lambda-serverless/services/getMessage.js
@@ -1,7 +1,7 @@
 const { TypedData } = require('ydb-sdk');
 const { CustomError } = require('../utils');
 
-const { errors: { MESSAGE_READ_ERROR }, RESPONSE_MAX_MESSAGES } = require('../constant');
+const { errors: { MESSAGE_READ_ERROR, ACCESS_DENIED, USER_CHECK_ERROR }, RESPONSE_MAX_MESSAGES } = require('../constant');
 
 const readMessages = async (session, logger) => {
     let messages;
@@ -18,6 +18,23 @@ const readMessages = async (session, logger) => {
     return messages;
 };
 
+const checkUserAccess = async (session, logger, token) => {
+    let tokenFoundNumber = 0;
+    try {
+        const query = `SELECT COUNT(*) AS tfn FROM user WHERE token = '${token}'`;
+        const prepareQuery = await session.prepareQuery(query);
+        const { resultSets } = await session.executeQuery(prepareQuery);
+        tokenFoundNumber = TypedData.createNativeObjects(resultSets[0])[0].tfn;
+    }
+    catch (e) {
+        logger.fatal(e);
+        throw new CustomError(USER_CHECK_ERROR);
+    }
+    if (tokenFoundNumber !== 1) {
+        throw new CustomError(ACCESS_DENIED);
+    }
+};
+
 const messageFilter = async (messages, lastReadMessageId) => {
 
     const lastMessageIndex = messages.reduce((lastInd, message, i) =>
@@ -32,10 +49,13 @@ const messageFilter = async (messages, lastReadMessageId) => {
 };
 
 module.exports = async (driver, logger, data) => {
-    const { queryParameters: { after } } = data;
+    const { queryParameters: { after, token } } = data;
+
     let filteredMessage = [];
 
     await driver.tableClient.withSession(async (session) => {
+        await checkUserAccess(session, logger, token);
+
         const allMessages = await readMessages(session, logger);
         filteredMessage = await messageFilter(allMessages, after)
     });
