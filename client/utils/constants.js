1b68772e41a5e71ab4ce96b6c60d7096a0b745c4
Maksim Tsipun <solcreative@gmail.com>
2022-01-27 23:06:27 +0300

    Refactoring | remove client from server repo

--- client/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const screens = {
-  CHAT: 'ChatScreen',
-  SETTINGS: 'SettingsScreen',
-  LOGIN: 'LoginScreen',
-  INFO: 'InfoScreen',
-}
-
-export const MAX_SHOWING_MESSAGES = 30;
-export const MESSAGE_FETCH_INTERVAL_MS = 5000;
--- client/utils/cultivateMessages.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-import { MAX_SHOWING_MESSAGES } from './constants';
-
-export default (storedMessages, incomingMessages, userName) => {
-    const preparedIncomingMessages = incomingMessages.map((m) => ({...m, self: m.userName === userName}));
-    const resultList = _.uniqBy([...storedMessages, ...preparedIncomingMessages], 'id');
-    resultList.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-    return resultList.splice(resultList.length - MAX_SHOWING_MESSAGES);
-}
